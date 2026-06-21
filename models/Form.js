31ee101dedd839283ccdc213f21bf808107fa82a
Toluwanimi Adenekan <tolugodsproperty@gmail.com>
2026-06-21 12:52:44 +0100

    added form code

---
+++ models/Form.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const formSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    }
+});
+
+module.exports = mongoose.model("Form", formSchema);
